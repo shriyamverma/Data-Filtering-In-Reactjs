f22cbed646819333e814186d1724d32ae7e7e91d
Shriyam Verma <shriyamverma@gmail.com>
2020-06-07 20:29:05 +0530

    SortBy, Display by status and Check by Brand working now

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -15,28 +15,99 @@ class Filter extends Component {
         }
         this.searchColor = this.searchColor.bind(this);
         this.searchBrand = this.searchBrand.bind(this);
+        this.changeStatus = this.changeStatus.bind(this);
+        this.selectBrand = this.selectBrand.bind(this);
+        this.sortBy = this.sortBy.bind(this);
         // this.paginate = this.paginate.bind(this);
     }
+
+    // shouldComponentUpdate() {
+    //     return false;
+    // }
+
+    // componentDidUpdate(prevProps, prevState) {
+    //     console.log(prevState.data);
+    //     console.log(this.state.data);
+    // }
     
-    searchColor(e){
+    searchColor = (e) => {
         // console.log(e.target.value);
         const newData = myData.filter(d => d.color.toLowerCase().includes(e.target.value.toLowerCase()));
         this.setState({data : newData});
     }
 
-    searchBrand(e){
+    searchBrand = (e) => {
         // console.log(e.target.value);
         const newData = myData.filter(d => d.brand.toLowerCase().includes(e.target.value.toLowerCase()));
         this.setState({data : newData});
     }
 
+    compareName = (a, b) => {
+        let comparison = 0;
+        if (a.name > b.name) {
+          comparison = 1;
+        } else if (a.name < b.name) {
+          comparison = -1;
+        }
+        return comparison;
+    }
+    compareColor = (a, b) => {
+        let comparison = 0;
+        if (a.color > b.color) {
+          comparison = 1;
+        } else if (a.color < b.color) {
+          comparison = -1;
+        }
+        return comparison;
+    }
+    compareBrand = (a, b) => {
+        let comparison = 0;
+        if (a.brand > b.brand) {
+          comparison = 1;
+        } else if (a.brand < b.brand) {
+          comparison = -1;
+        }
+        return comparison;
+    }
+
     sortBy = (e) => {
         // console.log(e.target.value);
-        if(e.target.value==="0"){
-            this.setState({data : myData});
+        let newData = myData;
+        if(e.target.value==="1"){
+            newData = myData.sort(this.compareName);
+        }
+        else if(e.target.value==="2"){
+            newData = myData.sort(this.compareColor);
+        }
+        else if(e.target.value==="3"){
+            newData = myData.sort(this.compareBrand);
+        }
+        this.setState({data : newData});
+    }
+
+    changeStatus = (e) => {
+        const el = document.getElementsByName('status');
+        if(el[0].checked) {
+            this.setState({data: myData.filter(d => {
+                return d.sold_out==='0';
+            })});
+        }
+        else if(el[1].checked) {
+            this.setState({data: myData.filter(d => {
+                return d.sold_out==='1';
+            })});
         }
     }
 
+    selectBrand = () => {
+        let newData = myData.filter(d => {
+            return document.getElementById(d.brand).checked;
+        })
+        if(newData.length===0){
+            newData = myData;
+        }
+        this.setState({data : newData});
+    }
     // paginate = (pgNo) => {
     //     // lastItem = pgNo * this.state.itemsPerPage;
     //     // firstItem = lastItem - this.state.itemsPerPage;
@@ -46,9 +117,10 @@ class Filter extends Component {
     render(){
         
         const UniqueBrands = Array.from(new Set(
-            this.state.data.map(d => d.brand)
+            myData.map(d => d.brand)
         )).sort();
 
+        console.log('In Render',this.state);
         return (
             <div className="row">
                 <div className="input-field col s12 m6">
@@ -68,14 +140,14 @@ class Filter extends Component {
                         <option value="3">Brand</option>
                     </select>
                 </div>
-                <div className="col s12 m6 center" style={{marginTop:'6px'}}>
+                <div className="col s12 m6 center" onChange={this.changeStatus} style={{marginTop:'6px'}}>
                     Display Only: &nbsp;
                     <label>
-                    <input className="with-gap" name="available" type="radio"/>
+                    <input className="with-gap" name="status" value="Available" type="radio"/>
                     <span>Available&nbsp;&nbsp;</span>
                     </label>
                     <label>
-                    <input className="with-gap" name="available" type="radio"/>
+                    <input className="with-gap" name="status" value="SoldOut" type="radio"/>
                     <span>Sold Out</span>
                     </label>
                 </div>
@@ -83,7 +155,7 @@ class Filter extends Component {
                 <div><br/>Check These Out:</div>
                 {UniqueBrands.map(brand => 
                         <label htmlFor brand style={{marginRight:"10px"}}>
-                            <input id={brand} type="checkbox" />
+                            <input id={brand} value={brand} type="checkbox" onClick={this.selectBrand}/>
                             <span>{brand}</span>
                         </label>
                     )}
